cc3567ae4cc384eaf9e60fca2f5314e45fb769f7
Jordan Lane <jordan.lane@steinias.com>
2020-04-16 13:10:22 -0400

    Refactor transitions

--- src/vue-web-sharer.vue
+++ src/vue-web-sharer.vue
@@ -15,18 +15,12 @@ export default {
   computed: {},
   methods: {
     hide() {
-      this.$el.classList.add("vue-web-sharer-transition-close");
-
-      setTimeout(() => {
-        this.$el.classList.remove("vue-web-sharer-transition-close");
-
-        this.$emit("close");
-      }, 200);
+      this.$emit("close");
     }
   },
   watch: {
     open(val) {
-      if (val === true) {
+      if (val) {
         if (navigator.share) {
           navigator
             .share({
@@ -54,42 +48,62 @@ export default {
 </script>
 
 <template>
-  <div :class="['vue-web-sharer', showFallback ? 'vue-web-sharer-open' : 'vue-web-sharer-close']">
-    <div class="vue-web-sharer-backdrop" @click="() => this.hide()"></div>
+  <transition name="fade">
+    <div v-if="showFallback" :class="['vue-web-sharer', ]">
+      <div class="vue-web-sharer-backdrop" @click="() => this.hide()"></div>
 
-    <div class="vue-web-sharer-action-sheet" @click="() => this.hide()">
-      <div class="vue-web-sharer-action-sheet-container">
-        <div class="vue-web-sharer-action-sheet-group">
-          <p class="vue-web-sharer-target">Test</p>
+      <div class="vue-web-sharer-action-sheet" @click="() => this.hide()">
+        <div class="vue-web-sharer-action-sheet-container">
+          <div class="vue-web-sharer-action-sheet-group">
+            <p class="vue-web-sharer-target">Test</p>
+            <p class="vue-web-sharer-target">Facebook</p>
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <style lang="postcss">
-.vue-web-sharer {
-  visibility: hidden;
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.2s;
+}
+.fade-enter,
+.fade-leave-to {
   opacity: 0;
 
-  cursor: pointer;
-  touch-action: manipulation;
-
-  & .vue-web-sharer-backdrop {
+  & .vue-web-sharer-backdrop,
+  & .vue-web-sharer-action-sheet {
     opacity: 0;
-    transition: opacity 0.1s linear;
-
-    background-color: var(--vue-web-sharer-backdrop-background, black);
-
-    z-index: var(--vue-web-sharer-zindex, 1000);
-    transform: translate3d(0, 0, 2px);
-
-    left: 0;
-    top: 0;
-    position: fixed;
-    height: 100%;
-    width: 100%;
   }
+
+  & .vue-web-sharer-action-sheet-group {
+    height: 0;
+  }
+}
+
+.vue-web-sharer {
+  cursor: pointer;
+  opacity: 1;
+  touch-action: manipulation;
+  visibility: visible;
+}
+
+.vue-web-sharer-backdrop {
+  opacity: var(--vue-web-sharer-backdrop-opacity, 0.15);
+  transition: opacity 0.1s linear;
+
+  background-color: var(--vue-web-sharer-backdrop-background, black);
+
+  z-index: var(--vue-web-sharer-zindex, 1000);
+  transform: translate3d(0, 0, 2px);
+
+  left: 0;
+  top: 0;
+  position: fixed;
+  height: 100%;
+  width: 100%;
 }
 
 .vue-web-sharer-target {
@@ -141,73 +155,47 @@ export default {
   position: fixed;
   z-index: calc(var(--vue-web-sharer-zindex, 1000) + 1);
   transform: translate3d(0, 0, 3px);
+  transition: opacity 0.2s ease-in;
+
+  opacity: 1;
 
   width: 100%;
   max-width: 540px;
-
-  & .vue-web-sharer-action-sheet-container {
-    display: flex;
-    flex-flow: column;
-    justify-content: flex-end;
-    height: 100%;
-    max-height: 100%;
-  }
-  & .vue-web-sharer-action-sheet-group {
-    box-shadow: var(
-      --vue-web-sharer-action-sheet-group-box-shadow,
-      0 0 8px 4px rgba(0, 0, 0, 0.1)
-    );
-
-    z-index: calc(var(--vue-web-sharer-zindex, 1000) + 10);
-    transform: translate3d(0, 0, 10px);
-
-    background: var(--vue-web-sharer-action-sheet-group-background, #fafafa);
-
-    display: flex;
-    justify-content: center;
-    flex-wrap: wrap;
-
-    height: 0;
-
-    transition-timing-function: cubic-bezier(0.36, 0.66, 0.04, 1);
-    transition: height 0.2s ease-in;
-
-    @media (max-width: 540px) {
-      border-radius: var(
-        --vue-web-sharer-action-sheet-group-border-radius,
-        8px 8px 0 0
-      );
-      justify-content: flex-start;
-    }
-  }
 }
 
-.vue-web-sharer-open {
-  visibility: visible;
-  opacity: 1;
-
-  & .vue-web-sharer-backdrop {
-    opacity: var(--vue-web-sharer-backdrop-opacity, 0.25);
-  }
-
-  & .vue-web-sharer-action-sheet {
-    opacity: 1;
-  }
+.vue-web-sharer-action-sheet-container {
+  display: flex;
+  flex-flow: column;
+  justify-content: flex-end;
+  height: 100%;
+  max-height: 100%;
 }
 
-.vue-web-sharer-open.vue-web-sharer-transition-close {
-  & .vue-web-sharer-backdrop {
-    opacity: 0;
-  }
-  & .vue-web-sharer-action-sheet-group {
-    height: 0;
-  }
-}
+.vue-web-sharer-action-sheet-group {
+  background: var(--vue-web-sharer-action-sheet-group-background, #fafafa);
+  box-shadow: var(
+    --vue-web-sharer-action-sheet-group-box-shadow,
+    0 0 8px 4px rgba(0, 0, 0, 0.1)
+  );
 
-.vue-web-sharer-open .vue-web-sharer-action-sheet-group {
+  z-index: calc(var(--vue-web-sharer-zindex, 1000) + 10);
+  transform: translate3d(0, 0, 10px);
+
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+
   height: var(--vue-web-sharer-height, 80px);
 
+  transition-timing-function: cubic-bezier(0.36, 0.66, 0.04, 1);
+  transition: height 0.2s;
+
   @media (max-width: 540px) {
+    border-radius: var(
+      --vue-web-sharer-action-sheet-group-border-radius,
+      8px 8px 0 0
+    );
+    justify-content: flex-start;
     height: var(--vue-web-sharer-height-small-device, 140px);
   }
 }
